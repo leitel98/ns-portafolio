406cf3eeefdcb855c0c26390209aab21bc9ed949
leitel98 <leitel1@uc.cl>
2023-04-27 09:34:05 -0400

    responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <main className='flex-flex-col tracking-normal'>
         <section id='#hero' className="flex flex-col">
           <div className="flex flex-col md:flex-row pt-32 pb-36 px-20 justify-between">
-            <h1 className="font-oxygen text-xl text-blackole max-w-lg md:w-1/2">Hi ! I’m <span className='inline-block text-pinkole'>Nicole</span>, UX/UI Designer, Problem solver, Artist and Crazy cat lady.</h1>
+            <h1 className="font-oxygen text-lg md:text-xl text-blackole max-w-lg md:w-1/2">Hi ! I’m <span className='inline-block text-pinkole'>Nicole</span>, UX/UI Designer, Problem solver, Artist and Crazy cat lady.</h1>
             <Image
               src={'/work-from-home.gif'}
               alt={''}
@@ -23,14 +23,14 @@ export default function Home() {
         </section>
 
         <section id="about" className="flex flex-col px-20 py-24 space-y-12 bg-cream bg-opacity-50 text-blackole">
-          <h1 className="text-2xl font-oxygen md:text-center">This is Me!</h1>
+          <h1 className="text-xl md:text-2xl font-oxygen md:text-center">This is Me!</h1>
           <p className="text-sm md:max-w-8xl md:text-center md:mx-auto">
             <span className="text-sm font-bold ">I am</span> a UX/UI designer, an artist and a natural problem solver. With a background in culinary arts and services, <span className="text-sm font-bold">I pride myself</span> in my ability to work as part of a  team with colleagues from diverse backgrounds. This has helped me develop an ease at sharing my ideas in a quick and simple way, in order to maintain a dynamic and synchronized work environment. <span className="text-sm font-bold">I believe</span> active listening is the first step to problem solving and effective communication. How can you find a solution if you don’t understand the problem? Ask the right questions, add some creativity and teamwork and you can deliver results that will make a lasting impact on people.
           </p>
         </section>
 
         <section id="work" className='pt-20 pb-36 px-10 md:px-20'>
-          <h1 className="text-xl mb-12 text-center md:text-left">
+          <h1 className="text-lg md:text-xl mb-12 text-center md:text-left">
             My Progress
           </h1>
           <div className=" grid gap-10 grid-cols-1 md:grid-cols-2 text-blackole">
@@ -43,36 +43,11 @@ export default function Home() {
                 height={100}
                 className='w-full'
               />
-              {/* <div className="absolute right-3 top-1/3 w-full">
-                <div className="relative ">
-                  <Image
-                    src={'/cel-1.png'}
-                    alt={''}
-                    width={105}
-                    height={105}
-                    className='absolute right-24'
-                  />
-                  <Image
-                    src={'/cel-2.png'}
-                    alt={''}
-                    width={75}
-                    height={75}
-                    className='absolute '
-                  />
-                  <Image
-                    src={'/cel-3.png'}
-                    alt={''}
-                    width={85}
-                    height={85}
-                    className=''
-                  />
-                </div>
-              </div> */}
               <div className='px-6'>
-                <h1 className="mt-10 font-rosario text-lg uppercase">
+                <h1 className="mt-10 font-rosario text-md md:text-lg uppercase">
                   red mujeres
                 </h1>
-                <p className="text-md mt-4">
+                <p className="text-sm md:text-md mt-4">
                   UX/UI design of a social network app just for women.
                 </p>
               </div>
@@ -106,10 +81,10 @@ export default function Home() {
                 />
               </div>
               <div className='px-6'>
-                <h1 className="mt-10 font-oxygen text-lg">
+                <h1 className="mt-10 font-oxygen text-md md:text-lg">
                   ART and HOBBIES
                 </h1>
-                <p className="text-md mt-4 font-rosario">
+                <p className="text-sm md:text-md mt-4 font-rosario">
                   Paintings, drawings, cakes and more.
                 </p>
               </div>
@@ -137,10 +112,10 @@ export default function Home() {
                 />
               </div>
               <div className='flex flex-col mt-10 md:ml-20 justify-center items-left'>
-                <h1 className=" font-oxygen text-lg text-left uppercase">
+                <h1 className=" font-oxygen text-md md:text-lg text-left uppercase">
                   CHILD OF WAR
                 </h1>
-                <p className="text-md mt-4 font-rosario">
+                <p className="text-sm md:text-md mt-4 font-rosario">
                   UX/UI design in a videogame demo. Available July 2023
                 </p>
               </div>
@@ -149,7 +124,7 @@ export default function Home() {
         </section>
 
         <footer id="contact" className="flex flex-col space-y-12 items-center py-10 px-20 bg-cream bg-opacity-60">
-          <h1 className="font-oxygen text-lg md:text-xl">
+          <h1 className="font-oxygen text-md md:text-lg">
             Get in contact with me
           </h1>
           <div className="flex flex-col justify-center items-center space-y-6 md:flex-row md:space-y-0 md:space-x-20">
